3b069146047dbb8a0d510369bb615e543e69e87a
ctdaibk09 <ctdai.bk09@gmail.com>
2016-08-30 00:57:27 +0700

    update: - check action type enhancers

--- share/enhancers.js
+++ share/enhancers.js
@@ -9,7 +9,27 @@ let enhancers = [
 
 if (process.env.NODE_ENV === 'development') {
   const DevTools = require('./components/DevTools.js').default;
-  enhancers.push(DevTools.instrument());
+  const actionConstants = require('./actionCreators/constants');
+
+  const checkActionType = (createStore) => (reducer, initialState, enhancer) => {
+    const stringActionTypes = Object
+      .keys(actionConstants)
+      .map(k => actionConstants[k]);
+
+    return createStore((state, action) => {
+      const type = typeof action === 'string' ?
+        action :
+        action.type;
+
+      if (!type) {
+        throw new Error('There is a typo in action type name.');
+      }
+
+      return reducer(state, action);
+    }, initialState, enhancer);
+  };
+
+  enhancers.push(checkActionType, DevTools.instrument());
 }
 
 export default compose.apply(null, enhancers);
